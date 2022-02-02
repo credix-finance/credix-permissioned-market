97fa980871a6d228d905e98d39d4249b70c8223e
Sushant Chandla <sushantchandla@gmail.com>
2022-02-02 14:08:06 +0530

    upadated setupscript

--- setup/setup.ts
+++ setup/setup.ts
@@ -53,7 +53,7 @@ async function init() {
   const liquidityPoolBaseTokenAccount =
     await utils.get_associated_token_address(baseMint, signingAuthorityPda);
 
-  /* await program.rpc.initializeMarket(
+  await program.rpc.initializeMarket(
     signingAuthorityBump,
     globalMarketStateBump,
     GLOBAL_MARKET_SEED,
@@ -75,10 +75,10 @@ async function init() {
       },
       signers: [lpTokenMintKeypair],
     }
-  ); */
+  );
 
   // Create the signing PDA for credix-permissioned-market
-  /*   const [pda_address, bump] = await PublicKey.findProgramAddress(
+  const [pda_address, bump] = await PublicKey.findProgramAddress(
     [Buffer.from(anchor.utils.bytes.utf8.encode("signing-authority"))],
     permissionedMarketProgram.programId
   );
@@ -97,16 +97,12 @@ async function init() {
     data: Buffer.from([255, bump]),
     programId: permissionedMarketProgram.programId,
   });
-  await provider.send(tx); */
-
-  const gm = await program.account.globalMarketState.fetch(
-    globalMarketStatePda
-  );
+  await provider.send(tx); 
 
   const [marketAPublicKey] = await listCredixMarket({
     connection: provider.connection,
     wallet: provider.wallet,
-    baseMint: gm.lpTokenMintAccount,
+    baseMint: lpTokenMintKeypair.publicKey,
     quoteMint: baseMint,
     baseLotSize: 10000,
     quoteLotSize: 10000,
